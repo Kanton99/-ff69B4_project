b49b56580fcd02a4539c37c8a01aafd8529cfe9e
PieMH <dondi.1797080@studenti.uniroma1.it>
2021-01-22 23:10:34 +0100

    Changed and moved IputControl

--- New Unity Project/Assets/Scripts/InputControl.cs
+++ New Unity Project/Assets/Scripts/InputControl.cs
@@ -9,10 +9,12 @@ public class InputControl : MonoBehaviour
     public SpriteRenderer arms;
     public SpriteRenderer body;
     public Rigidbody2D rigid;
+    public Vector2 direction;
 
     // Start is called before the first frame update
     void Start() {}
-
+    
+    /* ANOTHER METHOD TO MOVE THE CHARACTER
     // Update is called once per frame
     void Update() {
       Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0).normalized;
@@ -28,5 +30,26 @@ public class InputControl : MonoBehaviour
         animator.SetBool("run", false);
         rigid.velocity = Vector3.zero;
       }
+    }*/
+
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        if(direction != Vector2.zero) {
+            animator.SetBool("run", true);
+            bool flip = false;
+            if(Vector2.Dot(direction, new Vector2(1,0)) < 0)    flip = true;
+            arms.flipX = flip;
+            body.flipX = flip;
+        } 
+        else   animator.SetBool("run", false);
     }
+
+    void FixedUpdate() 
+    {
+        if(direction != Vector2.zero)   rigid.MovePosition(rigid.position + direction * moving_speed * Time.fixedDeltaTime);
+        else         rigid.velocity = Vector3.zero;
+    }
+
 }
