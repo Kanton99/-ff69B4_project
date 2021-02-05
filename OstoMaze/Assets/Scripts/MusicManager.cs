31721b3c212e2ec066f886882e88af4fe5301b27
PieMH <dondi.1797080@studenti.uniroma1.it>
2021-02-05 16:19:14 +0100

    changed project name, added mob prefab with animations and scripts, fixed minor bugs

---
+++ OstoMaze/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    AudioSource backgroundmusic;
+
+    void Start()
+    {
+        backgroundmusic = GetComponent<AudioSource>();
+        backgroundmusic.volume = 0;
+        backgroundmusic.PlayDelayed(0.1f);
+        StartCoroutine(FadeIn(backgroundmusic));
+    }
+
+    public void HubtoDungeon() {
+        StartCoroutine(FadeOut(backgroundmusic));
+    }
+    /*
+    public void DungeontoHub() {
+    }*/
+
+    private IEnumerator FadeOut(AudioSource music) {
+        float speed = 0.005f;
+        while(music.volume > 0) {
+            music.volume -= speed;
+            yield return new WaitForSeconds(0.1f);
+            if (music.volume < 0.05) {
+                music.Stop();
+                break;
+            }
+        }
+    }
+       private IEnumerator FadeIn(AudioSource music) {
+        float speed = 0.05f;
+        while(music.volume < 0.25) {
+            music.volume += speed;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
