3cfa53492fdbbe29aafe97f54266e1e753453501
Kanton99 <avolkov99@gmail.com>
2021-02-08 15:14:02 +0100

    cambi alla minimappa

--- OstoMaze/Assets/Scripts/Utils/MiniMapCamera.cs
+++ OstoMaze/Assets/Scripts/Utils/MiniMapCamera.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class MiniMapCamera : MonoBehaviour
 {
-    Vector3 p_pos;
+    Transform p_pos;
     private void Start()
     {
-        
+        p_pos = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void Update()
     {
-        p_pos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        gameObject.transform.position.Set(p_pos.x, p_pos.y, gameObject.transform.position.z);
+        gameObject.transform.position = p_pos.position;
     }
 }
