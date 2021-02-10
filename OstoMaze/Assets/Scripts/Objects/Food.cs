cf789ee39f98335c0e3c40efa63ad9d48d9451c5
PieMH <dondi.1797080@studenti.uniroma1.it>
2021-02-11 00:27:56 +0100

    animated food

---
+++ OstoMaze/Assets/Scripts/Objects/Food.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour, IInteractible
+{
+
+    public SpriteRenderer sprite;
+    public float TIMER;
+    private float _timer;
+
+    public bool interact() {
+        return true;
+    }
+
+    public void enterInteractionRange(GameObject gameObject) {
+        Debug.Log("Player in range!");
+    }
+
+    public void leaveInteractionRange() {
+
+    }
+
+    public GameObject getGameObject()
+    {
+        return this.gameObject;
+    }
+
+    public void Drop() {
+
+    }
+
+
+    void Start()
+    {
+        _timer = TIMER;
+    }
+
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0) Destroy(this.gameObject);
+    }
+}
