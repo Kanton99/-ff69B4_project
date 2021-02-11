45ca1ab76cb5b2fc52ed514426e5cb6134e9b85b
PieMH <dondi.1797080@studenti.uniroma1.it>
2021-02-11 13:31:31 +0100

    mobs now drop random food

--- OstoMaze/Assets/Scripts/Objects/Food.cs
+++ OstoMaze/Assets/Scripts/Objects/Food.cs
@@ -28,6 +28,15 @@ public class Food : MonoBehaviour, IInteractible
     
     public void Drop() {
         _timer = TIMER;
+        // a dirty Little secret in unity is that you can iterate Transform.
+        foreach(Transform child in this.transform) {
+            if (child.name == "Animated") { 
+                child.gameObject.AddComponent<SpriteRenderer>();
+                Sprite[] sprites = Resources.LoadAll<Sprite>("FoodSprites");
+                child.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+                child.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
+            }
+        }
     }
 
     void Update()
