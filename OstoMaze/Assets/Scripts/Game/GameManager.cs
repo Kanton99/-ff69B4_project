ea6f1b12eec9ffcb13bcfdefbfbfebf4a6179739
BioDegrado <ruixuan.xu@tutanota.com>
2021-02-11 16:34:04 +0100

    ok

--- OstoMaze/Assets/Scripts/Game/GameManager.cs
+++ OstoMaze/Assets/Scripts/Game/GameManager.cs
@@ -58,7 +58,7 @@ public class GameManager : MonoBehaviour
     }
 
     private void initHub() {
-        GameObject spawn_point = GameObject.FindWithTag("PlayerSpawn");
+        GameObject spawn_point = GameObject.FindWithTag("SpawnPoint");
         player.gameObject.transform.position = spawn_point.transform.position;
     }
 
