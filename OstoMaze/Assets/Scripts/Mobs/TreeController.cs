39fc4fbbf8fd78bda1f4bb20964abfd2882ca2ec
BioDegrado <ruixuan.xu@tutanota.com>
2021-02-11 18:04:15 +0100

    Added more sprites and more modifications for the boss

--- OstoMaze/Assets/Scripts/Mobs/TreeController.cs
+++ OstoMaze/Assets/Scripts/Mobs/TreeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TreeController : MonoBehaviour
 {
@@ -9,34 +10,24 @@ public class TreeController : MonoBehaviour
     public float _timer;
 
     public Animator mob_anim;
+    public UnityEvent dying_event;
 
     private Vector3 _offset;
 
     private Rigidbody2D _rb;
     private GameObject _player;
-    private AudioSource[] attacksounds;
+    private AudioSource[] _attacksounds;
     private SpriteRenderer _sprite;
     private Projectile _projectile;
 
-    public enum State {IDLE, MOVING, ATTACKING};
+    [SerializeField]
+    private int _hp = 0;
+
+    public enum State {SPAWNING, IDLE, MOVING, ATTACKING, DYING};
     public State _curr_state;
 
     public GameObject getPlayer() {
         return _player;
-    }
-
-    public void enterRange(GameObject player) {
-        mob_anim.SetBool("attack", true);
-        mob_anim.SetBool("walk", false);
-        _player = player;
-        _rb.velocity = Vector3.zero;
-        _curr_state = State.ATTACKING;
-    }
-
-    public void leaveRange() {
-        mob_anim.SetBool("attack", false);
-        mob_anim.SetBool("walk", false);
-        _curr_state = State.IDLE;
     }
 
     private void Attack()
@@ -49,15 +40,7 @@ public class TreeController : MonoBehaviour
             case 2:
                 break;
         }
-        attacksounds[Random.Range(0, 2)].Play();
-        Vector3 spawn = this.transform.position - new Vector3(0, 0.5f, 0);  // mob position - offset
-        Projectile projectile = Instantiate(this._projectile, spawn, new Quaternion(0,0,0,0));
-        projectile.transform.parent = null;
-        projectile.Shoot(spawn, _player.transform.position);
-    }
-
-    private void Shoot(Vector3 direction) {
-        attacksounds[Random.Range(0, 2)].Play();
+        _attacksounds[Random.Range(0, _attacksounds.Length)].Play();
         Vector3 spawn = this.transform.position - new Vector3(0, 0.5f, 0);  // mob position - offset
         Projectile projectile = Instantiate(this._projectile, spawn, new Quaternion(0,0,0,0));
         projectile.transform.parent = null;
@@ -65,32 +48,30 @@ public class TreeController : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         _rb = GetComponent<Rigidbody2D>();
-
         _offset = GetComponent<BoxCollider2D>().offset * transform.localScale.y;
         _sprite = GetComponent<SpriteRenderer>();
-        attacksounds = GetComponents<AudioSource>();
-        _curr_state = State.IDLE;
+        _attacksounds = GetComponents<AudioSource>();
         _projectile = Resources.Load<Projectile>("Projectile");
+
+        _curr_state = State.IDLE;
+    }
+
+    public void takeDamage(int damage) {
+        this._hp -= damage;
+        if(this._hp <= 0) {
+            dying_event.Invoke();
+        }
     }
 
     // Update is called once per frame
     void Update() {
         switch(_curr_state) {
+            case State.SPAWNING:
+                break;
             case State.IDLE:
                     mob_anim.SetBool("attack", false);
-                break;
-            case State.MOVING:
-                _timer -= Time.deltaTime;
-                if(_timer < 0) {
-                    _rb.velocity = Vector3.zero;
-                    _timer = TIMER;
-                    mob_anim.SetBool("walk", false);
-                    mob_anim.SetBool("attack", false);
-                    _curr_state = State.IDLE;
-                }
                 break;
             case State.ATTACKING:
                 if(Vector3.Dot(_player.transform.position - transform.position, transform.right) > 0)
